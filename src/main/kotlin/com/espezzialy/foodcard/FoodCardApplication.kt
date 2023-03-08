effd10c015a3a0deb74d258f9f3446d53015572d
Espezzialy Souza <espezzialy@outlook.com>
2023-03-08 11:33:21 -0300

    creating repository using axon initializer and adding dependencies

---
+++ src/main/kotlin/com/espezzialy/foodcard/FoodCardApplication.kt
@@ -0,0 +1,11 @@
+package com.espezzialy.foodcard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodCardApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodCardApplication>(*args)
+}
---
+++ src/test/kotlin/com/espezzialy/foodcard/FoodCardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.espezzialy.foodcard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoodCardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
